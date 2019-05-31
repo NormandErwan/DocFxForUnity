723d37643d39729bd48a83fb36fc53d7ed48bc0e
Erwan Normand <normand.erwan@protonmail.com>
2019-06-01 00:52:57 +0200

    Add a basic .NET program that iterates Unity versions

---
+++ UnityDocs/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using LibGit2Sharp;
+
+namespace UnityDocs
+{
+    class Program
+    {
+        private const string UnityRepoPath = "Unity";
+
+        static void Main(string[] args)
+        {
+            using (var repo = new Repository(UnityRepoPath))
+            {
+                foreach (var tag in repo.Tags)
+                {
+                    Console.WriteLine(tag.FriendlyName);
+                }
+            }
+        }
+    }
+}
