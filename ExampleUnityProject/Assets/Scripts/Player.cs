a2739d57024ea4d137657f7d7c7cfcb8eb797547
Erwan Normand <normand.erwan@protonmail.com>
2019-06-07 21:48:10 +0200

    :bento: Add a script to the example project

---
+++ ExampleUnityProject/Assets/Scripts/Player.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace NormandErwan.DocFxForUnity
+{
+    /// <summary>
+    /// The player of the game.
+    /// </summary>
+    public class Player : MonoBehaviour
+    {
+        [SerializeField]
+        private List<string> equipment;
+
+        [SerializeField]
+        private int startingHealth;
+
+        /// <summary>
+        /// Gets the list of the equipment carried by the player.
+        /// </summary>
+        public List<string> Equipment { get { return equipment; } }
+
+        /// <summary>
+        /// Gets the current health of the player.
+        /// </summary>
+        public int Health { get; private set; }
+
+        /// <summary>
+        /// Sets <see cref="Health"/> with <see cref="startingHealth"/>.
+        /// </summary>
+        protected virtual void Start()
+        {
+            Health = startingHealth;
+        }
+
+        /// <summary>
+        /// Deacreases <see cref="Health"/> by a specified value and display a game over if <see cref="Health"/> drops
+        /// to zero.
+        /// </summary>
+        /// <param name="value">How much to deacrease <see cref="Health"/>.</param>
+        public void Hit(int value)
+        {
+            Health -= value;
+
+            if (Health <= 0)
+            {
+                Debug.Log("GAME OVER");
+            }
+        }
+    }
+}
