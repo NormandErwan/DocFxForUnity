2d1f96ed2f6656269f3b769561ccd7d79ff63f76
Erwan Normand <normand.erwan@protonmail.com>
2020-04-04 15:57:55 +0200

    :fire: Remove Utils.GetSyncRepository()
    Duplicate of Git.GetSyncRepository()

--- UnityXrefMaps/Utils.cs
+++ UnityXrefMaps/Utils.cs
@@ -28,48 +28,6 @@ namespace DocFxForUnity
         }
 
         /// <summary>
-        /// Fetches changes and hard resets the specified repository to the latest commit of a specified branch. If no
-        /// repository is found, it will be cloned before.
-        /// </summary>
-        /// <param name="sourceUrl">The url of the repository.</param>
-        /// <param name="path">The directory path where to find/clone the repository.</param>
-        /// <param name="branch">The branch use on the repository.</param>
-        /// <returns>The synced repository on the latest commit of the specified branch.</returns>
-        public static Repository GetSyncRepository(string sourceUrl, string path, string branch = "master")
-        {
-            // Clone this repository to the specified branch if it doesn't exist
-            bool clone = !Directory.Exists(path);
-            if (clone)
-            {
-                Console.WriteLine($"Clonning {sourceUrl} to {path}");
-
-                var options = new CloneOptions() { BranchName = branch };
-                Repository.Clone(sourceUrl, path, options);
-            }
-
-            var repository = new Repository(path);
-
-            // Otherwise fetch changes and checkout to the specified branch
-            if (!clone)
-            {
-                Console.WriteLine($"Hard reset {path} to HEAD");
-                repository.Reset(ResetMode.Hard);
-                repository.RemoveUntrackedFiles();
-
-                Console.WriteLine($"Fetching changes from origin to {path}");
-                var remote = repository.Network.Remotes["origin"];
-                Commands.Fetch(repository, remote.Name, new string[0], null, null); // WTF is this API libgit2sharp?
-
-                Console.WriteLine($"Checking out {path} to {branch} branch");
-                Commands.Checkout(repository, branch);
-            }
-
-            Console.WriteLine();
-
-            return repository;
-        }
-
-        /// <summary>
         /// Run a command in a hidden window and returns its output.
         /// </summary>
         /// <param name="command">The command to run.</param>
